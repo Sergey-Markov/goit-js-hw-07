5240557eef95f5c78355bf12856b28dfa8db94ed
Sergey-Markov <1nativefl@gmail.com>
2021-05-21 11:02:45 +0300

    complete first task

--- homework-07/src/js/task-01.js
+++ homework-07/src/js/task-01.js
@@ -1,12 +1,13 @@
 
-// 1.Посчитает и выведет в консоль количество категорий в `ul#categories`, то есть
-// элементов `li.item`. Получится `'В списке 3 категории.'`.
+const listsOfItem = document.querySelectorAll('.item');
+console.log(listsOfItem.length);
 
-// 2. Для каждого элемента `li.item` в списке `ul#categories`, найдет и выведет в
-// консоль текст заголовка элемента (тега h2) и количество элементов в категории
-// (всех вложенных в него элементов `li`).
 
-// Например для первой категории получится:
+const everyGroupOfList = listsOfItem.forEach((el, index) => {
+    const category = el.querySelector('h2').textContent;
+    const countOfCategoryElements = el.querySelectorAll('li').length;
+    console.log(`- Категория: ${category}`);
+    console.log(`- Количество элементов: ${countOfCategoryElements}
+    `);
 
-// - Категория: Животные
-// - Количество элементов: 4
+});
