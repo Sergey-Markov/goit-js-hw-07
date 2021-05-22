2ac5356ade7d7851af22da612a9500b0ec2d2967
Sergey-Markov <1nativefl@gmail.com>
2021-05-22 11:41:24 +0300

    xyunya

--- homework-07/src/js/task-04.js
+++ homework-07/src/js/task-04.js
@@ -8,17 +8,24 @@
 // - Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
 const counterValue = 0;
-const spanEl = document.querySelector('#value').textContent = `${counterValue}`;
-console.log(counterValue);
+const spanEl = document.querySelector('#value');
+spanEl.textContent = `${counterValue}`
+console.log();
 const btnIncrement = document.querySelector('button[data-action="increment"]');
 console.log(btnIncrement);
 const btnDecrement = document.querySelector('button[data-action="decrement"]');
 
-function increment(counterValue) {
-    return counterValue += 1;
+function increment() {
+    const acc = counterValue + 1;
+    return spanEl.textContent = acc;
+    console.log(counterValue);
+};
+function decrement() {
+    return counterValue -= 1;
 };
 
-const decrement = (counterValue) => {
-    counterValue -= 1;
-};
+// const decrement = () => {
+//    return counterValue -= 1;
+// };
 btnIncrement.addEventListener('click', increment);
+btnDecrement.addEventListener('click', decrement);
