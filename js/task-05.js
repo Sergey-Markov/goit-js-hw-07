3257cee876457989f9603ce6b4cd213ba0706ec6
Sergey-Markov <1nativefl@gmail.com>
2021-05-24 16:21:21 +0300

    исправил по настоянию ментора, но как по мне, эта задача больше походит на решение через регулярные выражения

--- js/task-05.js
+++ js/task-05.js
@@ -5,8 +5,12 @@ const outputStrangersName = document.querySelector('#name-output');
 function makeOutput() {
     
     let outputName = outputStrangersName.textContent = this.value;
-    if (outputName !== '') {
-        return outputStrangersName.textContent = this.value;
+    // let stringValueWithoutSpace = outputName.split(' ').join(''); ===== я бы все же использовал такой подход
+    let stringValueWithoutSpace = outputName.trim();
+
+    console.log(stringValueWithoutSpace);
+    if (stringValueWithoutSpace !== '') {
+        return outputStrangersName.textContent = stringValueWithoutSpace;
     } else {
         outputStrangersName.textContent = 'незнакомец';
     };
